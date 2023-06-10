9096fea3e29acec47c2e86566576feb4593485f2
Trevor Coleman <trevor@trevorcoleman.design>
2023-06-10 14:56:31 -0400

    add prettierrc

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,38 @@
-import { parseArgs } from './args';
-import { glob } from 'glob';
-import { transformAndPrint } from './parse';
-import * as process from 'process';
-import { inquirerPrompt } from './inquirer';
-import { TransformerOptions } from './types/TransformerOptions';
-import { input, select } from '@inquirer/prompts';
-
+import { parseArgs } from "./args";
+import { glob } from "glob";
+import { transformAndPrint } from "./parse";
+import * as process from "process";
+import { inquirerPrompt } from "./inquirer";
+import { TransformerOptions } from "./types/TransformerOptions";
+import { input, select } from "@inquirer/prompts";
 
 async function main() {
-
   let files: string[] = [];
   let transformerOptions: TransformerOptions;
 
   if (process.argv.length > 2) {
     transformerOptions = parseArgs();
-
   } else {
     transformerOptions = await inquirerPrompt();
   }
 
- files = await glob(`${transformerOptions.directory}/**/*.ts?(x)`, {ignore: transformerOptions.ignore});
+  files = await glob(`${transformerOptions.directory}/**/*.ts?(x)`, {
+    ignore: transformerOptions.ignore,
+  });
 
   console.log(`
   Ready to begin? 
   
   BEFORE: 
-  import { ${transformerOptions.targetSymbols?.join(', ') ??
-             'MISSING TARGETS'}, baz } from '${transformerOptions.oldModuleName}';
+  import { ${
+    transformerOptions.targetSymbols?.join(", ") ?? "MISSING TARGETS"
+  }, baz } from '${transformerOptions.oldModuleName}';
   
   AFTER:
   import { baz } from '${transformerOptions.oldModuleName}'; 
-  import { ${transformerOptions.targetSymbols?.join(', ') ??
-             'MISSING TARGETS'} } from '${transformerOptions.newModuleName}';
+  import { ${
+    transformerOptions.targetSymbols?.join(", ") ?? "MISSING TARGETS"
+  } } from '${transformerOptions.newModuleName}';
   
   FILES: 
   ${transformerOptions.directory}/**/*.ts?(x)
@@ -39,23 +40,39 @@ async function main() {
   `);
 
   const proceed = await select({
-    message: 'Proceed?', choices: [
-      {name: 'Yes', value: 'yes', description: 'Begin transformation (Warning! Destructive!)'},
-      {name: 'Dry Run', value: 'dry-run', description: 'Print a list of files that will be changed'},
-      {name: 'CANCEL', value: 'cancel', description: 'Exit without making changes'},
+    message: "Proceed?",
+    choices: [
+      {
+        name: "Yes",
+        value: "yes",
+        description: "Begin transformation (Warning! Destructive!)",
+      },
+      {
+        name: "Dry Run",
+        value: "dry-run",
+        description: "Print a list of files that will be changed",
+      },
+      {
+        name: "CANCEL",
+        value: "cancel",
+        description: "Exit without making changes",
+      },
     ],
   });
 
-  if (proceed === 'cancel') {
-    console.log('Exiting...');
+  if (proceed === "cancel") {
+    console.log("Exiting...");
     process.exit(0);
   }
 
-  const makeChanges = proceed === 'yes';
+  const makeChanges = proceed === "yes";
 
-  await Promise.all(// files.map((file) => console.log(file))
-      files.map((file) => transformAndPrint(file, transformerOptions, makeChanges)));
+  await Promise.all(
+    // files.map((file) => console.log(file))
+    files.map((file) =>
+      transformAndPrint(file, transformerOptions, makeChanges)
+    )
+  );
 }
-
 
 main();
--- src/inquirer.ts
+++ src/inquirer.ts
@@ -1,85 +1,81 @@
-import { input, confirm } from '@inquirer/prompts';
-import { TransformerOptions } from './types/TransformerOptions';
-import fs from 'fs';
+import { input, confirm } from "@inquirer/prompts";
+import { TransformerOptions } from "./types/TransformerOptions";
+import fs from "fs";
 
-const BRIGHT_BLUE = '\x1b[94m';
-const NORMAL_CYAN = '\x1b[36m';
-const RESET = '\x1b[0m';
-const BRIGHT_WHITE = '\x1b[97m';
-const BRIGHT_BLUE_BG_WHITE_TEXT = '\x1b[44m\x1b[97m';
-
-
+const BRIGHT_BLUE = "\x1b[94m";
+const NORMAL_CYAN = "\x1b[36m";
+const RESET = "\x1b[0m";
+const BRIGHT_WHITE = "\x1b[97m";
+const BRIGHT_BLUE_BG_WHITE_TEXT = "\x1b[44m\x1b[97m";
 
 const WELCOME: string = `
 ${BRIGHT_BLUE_BG_WHITE_TEXT}TS${BRIGHT_WHITE}tevedore${RESET}: the TypeScript import refactoring tool!
 `;
 
-export async function inquirerPrompt():Promise<TransformerOptions> {
-   console.log(WELCOME);
+export async function inquirerPrompt(): Promise<TransformerOptions> {
+  console.log(WELCOME);
 
+  const identifiers = await getIdentifiers();
+  const oldModuleName = await getModuleName(
+    "What is the name of the OLD module the identifiers are currently exported from?"
+  );
+  const newModuleName = await getModuleName(
+    "What is the name of the NEW module the identifiers should be exported from?"
+  );
+  const directory = await getPath();
 
+  const shouldIgnoreNodeModules = await confirm({
+    message: "Ignore `node_modules/**`?",
+    default: true,
+  });
 
-   const identifiers = await getIdentifiers();
-   const oldModuleName = await getModuleName(
-       'What is the name of the OLD module the identifiers are currently exported from?');
-   const newModuleName = await getModuleName(
-       'What is the name of the NEW module the identifiers should be exported from?');
-   const directory = await getPath()
-
-   const shouldIgnoreNodeModules = await confirm({
-      message: 'Ignore \`node_modules/**\`?',
-      default: true,
-    });
-
-
-   return {
-      targetSymbols: identifiers,
-      oldModuleName,
-      newModuleName,
-      directory,
-      ignore: shouldIgnoreNodeModules ? ['**/node_modules/**'] : undefined,
-   }
+  return {
+    targetSymbols: identifiers,
+    oldModuleName,
+    newModuleName,
+    directory,
+    ignore: shouldIgnoreNodeModules ? ["**/node_modules/**"] : undefined,
+  };
 }
 
 async function getIdentifiers(): Promise<any> {
-   const identifiersInput = await input({
-      message: 'What identifier(s) do you want to target? (comma separated)',
-      validate: validateIdentifiers,
-   });
+  const identifiersInput = await input({
+    message: "What identifier(s) do you want to target? (comma separated)",
+    validate: validateIdentifiers,
+  });
 
-   return identifiersInput.split(',').map((v) => v.trim());
+  return identifiersInput.split(",").map((v) => v.trim());
 }
 
-async function getModuleName(message:string): Promise<any> {
-   let oldModuleNameInput = await input({
-      message,
-      validate: (value) => {
-         return isValidModuleImportPath(value) ? true : 'Invalid module name';
-      },
-   });
+async function getModuleName(message: string): Promise<any> {
+  let oldModuleNameInput = await input({
+    message,
+    validate: (value) => {
+      return isValidModuleImportPath(value) ? true : "Invalid module name";
+    },
+  });
 
-   if (isRelativeModuleImportPath(oldModuleNameInput)) {
-      const relativePathIsOk = await confirm({
-         message: `${oldModuleNameInput} appears to be a relative path. Is that correct?`,
-         default: true,
-      })
+  if (isRelativeModuleImportPath(oldModuleNameInput)) {
+    const relativePathIsOk = await confirm({
+      message: `${oldModuleNameInput} appears to be a relative path. Is that correct?`,
+      default: true,
+    });
 
-      if (!relativePathIsOk) {
-         oldModuleNameInput = await getModuleName(message);
-      }
-   }
+    if (!relativePathIsOk) {
+      oldModuleNameInput = await getModuleName(message);
+    }
+  }
 
-   return oldModuleNameInput;
+  return oldModuleNameInput;
 }
 
 async function getPath(): Promise<string> {
+  const path = await input({
+    message: "What is the path to the directory you want to target?",
+    validate: isValidDirectory,
+  });
 
-   const path = await input({
-      message: 'What is the path to the directory you want to target?',
-      validate: isValidDirectory,
-   });
-
-   return path;
+  return path;
 }
 
 /**
@@ -92,7 +88,7 @@ async function getPath(): Promise<string> {
  * @param {string} str - The string to check.
  * @returns {boolean} Returns true if the string is a valid identifier, else false.
  */
-function isValidIdentifier(str:string) {
+function isValidIdentifier(str: string) {
   const identifierRegex = /^[a-zA-Z_$][a-zA-Z0-9_$]*$/;
 
   return identifierRegex.test(str) && !reservedKeywords.includes(str);
@@ -100,101 +96,101 @@ function isValidIdentifier(str:string) {
 
 // A list of reserved keywords in JavaScript/TypeScript
 const reservedKeywords = [
-   'abstract',
-   'any',
-   'as',
-   'async',
-   'await',
-   'boolean',
-   'break',
-   'case',
-   'catch',
-   'class',
-   'const',
-   'continue',
-   'debugger',
-   'declare',
-   'default',
-   'delete',
-   'do',
-   'else',
-   'enum',
-   'export',
-   'extends',
-   'false',
-   'finally',
-   'for',
-   'from',
-   'function',
-   'get',
-   'if',
-   'implements',
-   'import',
-   'in',
-   'infer',
-   'instanceof',
-   'interface',
-   'is',
-   'keyof',
-   'let',
-   'module',
-   'namespace',
-   'never',
-   'new',
-   'null',
-   'number',
-   'object',
-   'of',
-   'package',
-   'private',
-   'protected',
-   'public',
-   'readonly',
-   'require',
-   'return',
-   'set',
-   'static',
-   'string',
-   'super',
-   'switch',
-   'symbol',
-   'this',
-   'throw',
-   'true',
-   'try',
-   'type',
-   'typeof',
-   'unique',
-   'unknown',
-   'var',
-   'void',
-   'while',
-   'with',
-   'yield'
+  "abstract",
+  "any",
+  "as",
+  "async",
+  "await",
+  "boolean",
+  "break",
+  "case",
+  "catch",
+  "class",
+  "const",
+  "continue",
+  "debugger",
+  "declare",
+  "default",
+  "delete",
+  "do",
+  "else",
+  "enum",
+  "export",
+  "extends",
+  "false",
+  "finally",
+  "for",
+  "from",
+  "function",
+  "get",
+  "if",
+  "implements",
+  "import",
+  "in",
+  "infer",
+  "instanceof",
+  "interface",
+  "is",
+  "keyof",
+  "let",
+  "module",
+  "namespace",
+  "never",
+  "new",
+  "null",
+  "number",
+  "object",
+  "of",
+  "package",
+  "private",
+  "protected",
+  "public",
+  "readonly",
+  "require",
+  "return",
+  "set",
+  "static",
+  "string",
+  "super",
+  "switch",
+  "symbol",
+  "this",
+  "throw",
+  "true",
+  "try",
+  "type",
+  "typeof",
+  "unique",
+  "unknown",
+  "var",
+  "void",
+  "while",
+  "with",
+  "yield",
 ];
 
-
-function validateIdentifiers (value:string)  {
-   const identifiers = value.split(',').map((v) => v.trim());
-   const invalidIdentifiers = identifiers.filter((id) => !isValidIdentifier(id));
-   return invalidIdentifiers.length === 0 ? true : `Invalid identifier(s): ${invalidIdentifiers.join(', ')}`;
+function validateIdentifiers(value: string) {
+  const identifiers = value.split(",").map((v) => v.trim());
+  const invalidIdentifiers = identifiers.filter((id) => !isValidIdentifier(id));
+  return invalidIdentifiers.length === 0
+    ? true
+    : `Invalid identifier(s): ${invalidIdentifiers.join(", ")}`;
 }
 
-
 function isValidModuleImportPath(importPath: string): boolean {
-   const moduleImportRegex = /^(@?[a-zA-Z0-9_./-]+)+$/;
-   return moduleImportRegex.test(importPath);
+  const moduleImportRegex = /^(@?[a-zA-Z0-9_./-]+)+$/;
+  return moduleImportRegex.test(importPath);
 }
 
 function isRelativeModuleImportPath(importPath: string): boolean {
-   return /^\.\.?\//.test(importPath);
+  return /^\.\.?\//.test(importPath);
 }
 
-function isValidDirectory(input:string) {
-   const path = `${process.cwd()}/${input}`;
-   if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
-      return true;
-   } else {
-      return 'Please enter a valid directory path';
-   }
+function isValidDirectory(input: string) {
+  const path = `${process.cwd()}/${input}`;
+  if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
+    return true;
+  } else {
+    return "Please enter a valid directory path";
+  }
 }
